3cb3ee78c1071bc92f96befe6b00ad33a089b315
Crypnotic <crypnoticofficial@gmail.com>
2017-09-13 14:45:31 -0500

    Update to recommended Forge version#2478

--- src/main/java/me/crypnotic/haptic/Haptic.java
+++ src/main/java/me/crypnotic/haptic/Haptic.java
@@ -14,7 +14,7 @@ public class Haptic {
 
 	public static final String MODID = "haptic";
 	public static final String NAME = "Haptic";
-	public static final String VERSION = "1.12-2.1.1";
+	public static final String VERSION = "1.12-2.1.2";
 	public static final Logger LOGGER = LogManager.getLogManager().getLogger(MODID);
 
 	@SidedProxy(clientSide = "me.crypnotic.haptic.proxies.ClientProxy")
